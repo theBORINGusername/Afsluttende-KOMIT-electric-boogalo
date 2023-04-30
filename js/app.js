9abac4c5eab808457d4c8925946bd7cd193f37be
theBORINGusername <42040416+theBORINGusername@users.noreply.github.com>
2023-04-30 20:55:09 +0200

    Idk

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 const html = document.querySelector('html');
 const nav = document.querySelector('.nav');
-const first_section = document.querySelector('#section-first');
 
 var height = nav.clientHeight;
-console.log(height);
 
 first_section.style = `padding-top: ${height}px;`;
-
 html.style = `scroll-padding-top: ${height}px;`;
---
+++ js/game-level.js
@@ -0,0 +1,6 @@
+const first_section = document.querySelector('#section-first');
+const progress_element = document.querySelector('.progress-bar-progress');
+
+var progress = 10;
+progress_element.style = `width: ${progress}%;`;
+console.log(progress_element.style)
